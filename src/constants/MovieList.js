437dfde7ed47d84216fd79fc0dbff11c4c6e7d36
Vladyslav <kramarenko.creator@gmail.com>
2023-10-15 20:28:48 +0300

    Added movies

--- src/constants/MovieList.js
+++ src/constants/MovieList.js
@@ -1,7 +1,67 @@
 const movies = [
 	{
-		title: 'Focus',
+		title: 'Get Out',
+		genre: 'Detective',
+		year: 2017,
+		rating: 8.8,
+		duration: '1:44',
+		description:
+			"Is a gripping horror-thriller that delves into racial stereotypes and fears as a young African American man uncovers a terrifying family secret during a visit to his girlfriend's house.",
+		image: 'https://www.nofi.media/wp-content/uploads/2017/08/getout_A-672x378.jpg',
+	},
+	{
+		title: 'Inception',
+		genre: 'Thriller',
+		year: 2010,
+		rating: 9.5,
+		duration: '2:28',
+		description:
+			"Inception is a mind-bending science fiction thriller where thieves enter dreams to steal secrets. Dom Cobb, an expert extractor, must plant an idea in someone's subconscious, blurring the lines between dreams and reality.",
+		image: 'https://ntvb.tmsimg.com/assets/p7825626_v_h10_aa.jpg?w=1280&h=720',
+	},
+	{
+		title: 'Super Alibi.com',
 		genre: 'Comedy',
+		year: 2017,
+		rating: 8.5,
+		duration: '1:30',
+		description:
+			"Super Alibi.com is a French comedy film about a man who works for a company that creates alibis, but he gets into a tricky situation that challenges his reputation.",
+		image: 'https://static.etvnet.com/shared/albums/media/11/1111288-superalibi-lg.jpg',
+	},
+	{
+		title: 'The age of Adeline',
+		genre: 'Drama',
+		year: 2015,
+		rating: 8.9,
+		duration: '1:52',
+		description:
+			"The Age of Adaline is a romantic fantasy film about a woman who stops aging after an accident, leading to a solitary life until she meets a man who could reveal her secret",
+		image: 'https://www.comfortenglish.com/wp-content/uploads/2017/01/Adaline_FB_Image.jpg',
+	},
+	{
+		title: 'Oxygen',
+		genre: 'Thriller',
+		year: 2021,
+		rating: 8.0,
+		duration: '1:40',
+		description:
+			"In this gripping psychological thriller, a young woman awakens in a cryogenic chamber, surrounded by wires and medical equipment, with no recollection of how she became ensnared in this claustrophobic nightmare.",
+		image: 'https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABQUqVou4d5Za_6mP4xU0jqJwxYEgAVPavUryCEaW0RRP-42yRM1R_G-hgVUBEw6rsRbJhQ6sQYNnlEAnA9YiV1vsWb8bk8FzTF1N.jpg?r=0dc',
+	},
+	{
+		title: 'Knives Out',
+		genre: 'Detective',
+		year: 2019,
+		rating: 10.0,
+		duration: '2:10',
+		description:
+			"Knives Out is a 2019 mystery-comedy film directed by Rian Johnson. The story centers on the investigation of a famous crime novelist's death and features dark humor, plot twists, and social commentary.",
+		image: 'https://m.media-amazon.com/images/S/pv-target-images/1e1a12cc40bc3f3d229459fc70e8bdcd317efa9f3eb083b3618a75d7cb2432e9.jpg',
+	},
+	{
+		title: 'Focus',
+		genre: 'Criminal',
 		year: 2015,
 		rating: 8.3,
 		duration: '1:45',
@@ -19,16 +79,7 @@ const movies = [
 			'In the magical land of Barbiland, perfection is the norm, but Barbie stands out. She embarks on a journey into the human world, where she learns that happiness demands a unique set of qualities.',
 		image: 'https://cloudfront-us-east-1.images.arcpublishing.com/gray/O3NYSSN6ENBOVKXMWONKBPHDZA.jpg',
 	},
-	{
-		title: 'Interstellar',
-		genre: 'Drama',
-		year: 2014,
-		rating: 10.0,
-		duration: '2:49',
-		description:
-			'In the movie, a team of explorers searches for a new human habitat in space due to an Earth environmental crisis, facing black holes, mysterious planets, and unique challenges.',
-		image: 'https://i.pinimg.com/originals/08/e9/03/08e90309165a459a934716026383792e.jpg',
-	},
+	
 	{
 		title: 'Top Gun Maverick',
 		genre: 'History',
@@ -39,66 +90,7 @@ const movies = [
 			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
 		image: 'https://www.niwaju.com/wp-content/uploads/2023/01/lecons-business-top-gun-maverick-1140x641.jpg',
 	},
-	{
-		title: 'Top Gun Maverick',
-		genre: 'History',
-		year: 2022,
-		rating: 9.0,
-		duration: '2:11',
-		description:
-			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
-		image: 'https://www.iloveimg.com/img/iloveimg/social/ru/iloveimg.jpg',
-	},
-	{
-		title: 'Top Gun Maverick',
-		genre: 'History',
-		year: 2022,
-		rating: 9.0,
-		duration: '2:11',
-		description:
-			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
-		image: 'https://ik.imagekit.io/ikmedia/backlit.jpg',
-	},
-	{
-		title: 'Top Gun Maverick',
-		genre: 'History',
-		year: 2022,
-		rating: 9.0,
-		duration: '2:11',
-		description:
-			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
-		image: 'https://www.imgacademy.com/sites/default/files/styles/scale_1700w/public/2022-07/img-homepage-meta_0.jpg?itok=LMirU0Ik',
-	},
-	{
-		title: 'Top Gun Maverick',
-		genre: 'History',
-		year: 2022,
-		rating: 9.0,
-		duration: '2:11',
-		description:
-			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
-		image: 'https://www.imgonline.com.ua/examples/bee-on-daisy.jpg',
-	},
-	{
-		title: 'Top Gun Maverick',
-		genre: 'History',
-		year: 2022,
-		rating: 9.0,
-		duration: '2:11',
-		description:
-			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
-		image: 'https://cdnn11.img.sputnik.by/img/07e5/09/01/1056115789_136:0:2867:2048_1920x0_80_0_0_ace5fe8ad699e1c61f1c1f92e59fc88f.jpg',
-	},
-	{
-		title: 'Top Gun Maverick',
-		genre: 'History',
-		year: 2022,
-		rating: 9.0,
-		duration: '2:11',
-		description:
-			'Maverick, a highly skilled test pilot for over three decades, dedicated himself to constant self-improvement. However, he refrained from pursuing rank advancement, as it would have meant giving up his passion for flying',
-		image: 'https://www.imgonline.com.ua/examples/random-pixels-wallpaper-big.jpg',
-	},
+	
 ];
 
 export default movies;
